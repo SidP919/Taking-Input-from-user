4a2597d543c3a2489f42f1fe8d53afbc83afd47f
SidP0008 <sidp0008@gmail.com>
2020-01-18 17:11:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+x = input()
+y = input()
+z = int(x) + int(y)
+print(z)
+print()
+a = int(input('Enter another no: '))
+b = int(input('Enter one more no: '))
+c = a + b
+print(c)
+print()
+calculator = eval(input("Enter an expression to calculate: "))
+print(calculator)
+print()
+
+# #for command line arguments
+# import sys
+# p = int(sys.argv[1]) #bcoz argv[0] contains filename
+# q = int(sys.argv[2])
+# r = p + q
+# print(r)
